d25d60fb462594b45d16234dce0b7007afbf89aa
mitchmiller08 <mitchmiller08@users.noreply.github.com>
2018-08-26 14:06:38 -0700

    Create gen_training.py
    Placeholder

---
+++ model_training/gen_training.py
@@ -0,0 +1,9 @@
+"""
+This module is used to generate labeled training images from video files.
+
+A video file is loaded and split into individual frames. Each frame will have a set of distortions
+applied to it and the resulting images will be saved back to disk for model training.
+
+Module author: Mitch Miller <mitch.miller08@gmail.com>
+
+"""
