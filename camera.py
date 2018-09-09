b54a0176e8ceb2ca0958d417bce48e986cc903a9
mitchmiller08 <mitch.miller08@gmail.com>
2018-09-09 15:52:37 -0700

    update predictor background process method

--- camera.py
+++ camera.py
@@ -6,8 +6,6 @@ frame is captured to establish state. State is recalculated after a fixed
 number of frames have been collected. A jpg encoded image is returned from every
 call of VideoCamera.get_frame().
 
-# TODO: Rescale frames to 150x150 for prediction
-
 Module author: Mitch Miller <mitch.miller08@gmail.com>
 
 """
@@ -15,7 +13,11 @@ import cv2
 import pickle
 import numpy as np
 
+from multiprocessing import Process, Queue
+
 from keras.models import load_model
+
+from model_training.train_model import img_width, img_height
 
 class VideoCamera(object):
     """A class for collecting images from a hardware camera
@@ -23,32 +25,38 @@ class VideoCamera(object):
     Attributes:
         model (:obj:keras.Sequential): The classification model used to predict
             image state.
-        label_map (dict): A dictionary describing the mapping from predicted
-            values to their corresponding label.
 
         capture (:obj:cv2.VideoCapture): The OpenCV video capture object.
-        current_frame (:obj:numpy.ndarray): A numpy array of image pixel values.
-            Three channels, one per RGB.
+        _frame_count (int): The number of frames collected since the last state
+            calculation.
 
-        state (int): An integer representing the state predicted by the
-            classifier.
-        state_label (str): A text label of the current state.
+        frame_q (:obj:multiprocessing.Queue): A queue of frames to be used for
+            state calculations
+        state_q (:obj:multiprocessing.Queue): A queue of recent state
+            calculation results
+        predictor (:obj:multiprocessing.Process): A background process for the
+            state calculations.
         STATE_REFRESH (int): How many frames to pass before recalculating state.
 
     """
     STATE_REFRESH = 60
 
     def __init__(self, model_path, label_map_path):
+        ## Model attributes
         self.model = load_model(model_path)
-        with open(label_map_path, 'rb') as label_file:
-            self.label_map = pickle.load(label_file)
 
+        ## Capture attributes
         self.capture = cv2.VideoCapture(0)
         self._frame_count = 0
-        _, self.current_frame = self.capture.read()
-        self._predict_state()
+
+        ## Predictor attributes
+        self.frame_q = Queue()
+        self.state_q = Queue()
+        self.predictor = Process(target=self._predict_state)
+        self.predictor.start()
 
     def __del__(self):
+        self.predictor.terminate()
         self.capture.release()
 
     def get_frame(self):
@@ -58,18 +66,28 @@ class VideoCamera(object):
         of frames have already been collected, recalculate the current state.
 
         """
-        _, self.current_frame = self.capture.read()
+        ## Get current frame from camera
+        _, current_frame = self.capture.read()
         self._frame_count += 1
-        if self._frame_count == self.STATE_REFRESH:
-            self._predict_state()
+        ## Check if time to recompute state and old state calculation is done
+        if self._frame_count >= self.STATE_REFRESH\
+        and self.frame_q.empty():
+            self.frame_q.put(current_frame)
             self._frame_count = 0
-        _, jpeg = cv2.imencode('.jpg', self.current_frame)
+        ## Encode frame as .jpg
+        _, jpeg = cv2.imencode('.jpg', current_frame)
         return jpeg.tobytes()
 
     def _predict_state(self):
         """Predict the current state using the classification model."""
-        X = self.current_frame
-        X_rescale = x * 1./255
+        ## Get frame from queue
+        X = self.frame_q.get()
+        ## Rescale image to model parameters
+        X = cv2.resize(X, dsize=(img_width, img_height),
+                       interpolation=cv2.INTER_LINEAR)
+        X_rescale = X * 1./255
         X_rescale = np.expand_dims(X_rescale, axis=0)
-        self.state = self.model.predict_classes(X_rescale)
-        self.state_label = self.label_map[self.state]
+        current_state = self.model.predict_classes(X_rescale)
+        ## Discard oldest state
+        _ = self.state_q.get()
+        self.state_q.put(current_state)
